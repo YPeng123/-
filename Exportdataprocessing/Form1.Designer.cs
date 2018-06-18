7f0464b0d312e14a13e841178b60197fe2c3035f
易壹蓝 <jiaypeng@qq.com>
2018-06-19 00:36:50 +0800

    将数据导入数据库

---
+++ Exportdataprocessing/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Exportdataprocessing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urlDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.skunumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.minpriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maxpriceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.minchengjiaoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.minyishouDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.reviewquantityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.keywordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.marketDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.yiyilandbDataSet = new Exportdataprocessing.yiyilandbDataSet();
+            this.productsTableAdapter = new Exportdataprocessing.yiyilandbDataSetTableAdapters.productsTableAdapter();
+            this.btndaoru = new System.Windows.Forms.Button();
+            this.btnrefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yiyilandbDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.urlDataGridViewTextBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.skunumDataGridViewTextBoxColumn,
+            this.minpriceDataGridViewTextBoxColumn,
+            this.maxpriceDataGridViewTextBoxColumn,
+            this.minchengjiaoDataGridViewTextBoxColumn,
+            this.minyishouDataGridViewTextBoxColumn,
+            this.reviewquantityDataGridViewTextBoxColumn,
+            this.keywordDataGridViewTextBoxColumn,
+            this.marketDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.productsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(472, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(304, 386);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // urlDataGridViewTextBoxColumn
+            // 
+            this.urlDataGridViewTextBoxColumn.DataPropertyName = "url";
+            this.urlDataGridViewTextBoxColumn.HeaderText = "url";
+            this.urlDataGridViewTextBoxColumn.Name = "urlDataGridViewTextBoxColumn";
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "title";
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            // 
+            // skunumDataGridViewTextBoxColumn
+            // 
+            this.skunumDataGridViewTextBoxColumn.DataPropertyName = "skunum";
+            this.skunumDataGridViewTextBoxColumn.HeaderText = "skunum";
+            this.skunumDataGridViewTextBoxColumn.Name = "skunumDataGridViewTextBoxColumn";
+            // 
+            // minpriceDataGridViewTextBoxColumn
+            // 
+            this.minpriceDataGridViewTextBoxColumn.DataPropertyName = "minprice";
+            this.minpriceDataGridViewTextBoxColumn.HeaderText = "minprice";
+            this.minpriceDataGridViewTextBoxColumn.Name = "minpriceDataGridViewTextBoxColumn";
+            // 
+            // maxpriceDataGridViewTextBoxColumn
+            // 
+            this.maxpriceDataGridViewTextBoxColumn.DataPropertyName = "maxprice";
+            this.maxpriceDataGridViewTextBoxColumn.HeaderText = "maxprice";
+            this.maxpriceDataGridViewTextBoxColumn.Name = "maxpriceDataGridViewTextBoxColumn";
+            // 
+            // minchengjiaoDataGridViewTextBoxColumn
+            // 
+            this.minchengjiaoDataGridViewTextBoxColumn.DataPropertyName = "minchengjiao";
+            this.minchengjiaoDataGridViewTextBoxColumn.HeaderText = "minchengjiao";
+            this.minchengjiaoDataGridViewTextBoxColumn.Name = "minchengjiaoDataGridViewTextBoxColumn";
+            // 
+            // minyishouDataGridViewTextBoxColumn
+            // 
+            this.minyishouDataGridViewTextBoxColumn.DataPropertyName = "minyishou";
+            this.minyishouDataGridViewTextBoxColumn.HeaderText = "minyishou";
+            this.minyishouDataGridViewTextBoxColumn.Name = "minyishouDataGridViewTextBoxColumn";
+            // 
+            // reviewquantityDataGridViewTextBoxColumn
+            // 
+            this.reviewquantityDataGridViewTextBoxColumn.DataPropertyName = "review_quantity";
+            this.reviewquantityDataGridViewTextBoxColumn.HeaderText = "review_quantity";
+            this.reviewquantityDataGridViewTextBoxColumn.Name = "reviewquantityDataGridViewTextBoxColumn";
+            // 
+            // keywordDataGridViewTextBoxColumn
+            // 
+            this.keywordDataGridViewTextBoxColumn.DataPropertyName = "keyword";
+            this.keywordDataGridViewTextBoxColumn.HeaderText = "keyword";
+            this.keywordDataGridViewTextBoxColumn.Name = "keywordDataGridViewTextBoxColumn";
+            // 
+            // marketDataGridViewTextBoxColumn
+            // 
+            this.marketDataGridViewTextBoxColumn.DataPropertyName = "market";
+            this.marketDataGridViewTextBoxColumn.HeaderText = "market";
+            this.marketDataGridViewTextBoxColumn.Name = "marketDataGridViewTextBoxColumn";
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "products";
+            this.productsBindingSource.DataSource = this.yiyilandbDataSet;
+            // 
+            // yiyilandbDataSet
+            // 
+            this.yiyilandbDataSet.DataSetName = "yiyilandbDataSet";
+            this.yiyilandbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // btndaoru
+            // 
+            this.btndaoru.Location = new System.Drawing.Point(51, 54);
+            this.btndaoru.Name = "btndaoru";
+            this.btndaoru.Size = new System.Drawing.Size(75, 23);
+            this.btndaoru.TabIndex = 1;
+            this.btndaoru.Text = "导入";
+            this.btndaoru.UseVisualStyleBackColor = true;
+            this.btndaoru.Click += new System.EventHandler(this.btndaoru_Click);
+            // 
+            // btnrefresh
+            // 
+            this.btnrefresh.Location = new System.Drawing.Point(132, 54);
+            this.btnrefresh.Name = "btnrefresh";
+            this.btnrefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnrefresh.TabIndex = 2;
+            this.btnrefresh.Text = "刷新";
+            this.btnrefresh.UseVisualStyleBackColor = true;
+            this.btnrefresh.Click += new System.EventHandler(this.btnrefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 500);
+            this.Controls.Add(this.btnrefresh);
+            this.Controls.Add(this.btndaoru);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yiyilandbDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private yiyilandbDataSet yiyilandbDataSet;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private yiyilandbDataSetTableAdapters.productsTableAdapter productsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn urlDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn skunumDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn minpriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maxpriceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn minchengjiaoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn minyishouDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn reviewquantityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn keywordDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn marketDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button btndaoru;
+        private System.Windows.Forms.Button btnrefresh;
+    }
+}
+
---
+++ Exportdataprocessing/Form1.cs
@@ -0,0 +1,179 @@
+﻿using ExcelDataReader;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exportdataprocessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“yiyilandbDataSet.products”中。您可以根据需要移动或删除它。
+            this.productsTableAdapter.Fill(this.yiyilandbDataSet.products);
+        }
+
+        private DataTable maintable = null;
+
+        private string 哪个市场(string url)
+        {
+            if (string.IsNullOrEmpty(url)) return "未知";
+            if (url.Contains("detail")) return "天猫";
+            else if (url.Contains("taobao")) return "淘宝";
+            return "未知";
+        }
+
+        private void DaoRu()
+        {
+            Form2 form2 = new Form2();
+            if (form2.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            //导入
+            OpenFileDialog form = new OpenFileDialog();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                var stream = form.OpenFile();
+                string extension = Path.GetExtension(form.FileName);
+                IExcelDataReader excelReader = null;
+                if (extension == ".xls")
+                {
+                    excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+                else if (extension == ".xlsx")
+                {
+                    excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                }
+                //将数据转为datatable
+                maintable = excelReader.AsDataSet().Tables[0];
+                foreach (DataRow row in maintable.Rows)
+                {
+                    string id = row[Form2.idcolindex]?.ToString();
+                    string url = row[Form2.urlcolindex]?.ToString();
+                    string title = row[Form2.titlecolindex]?.ToString();
+                    string market = 哪个市场(url);
+                    decimal minprice;
+                    decimal maxprice;
+                    ParsePrice(row[Form2.pricecolindex]?.ToString(), out minprice, out maxprice);
+                    int chengjiaonum;
+                    if (!int.TryParse(row[Form2.chengjiaocolindex]?.ToString(), out chengjiaonum))
+                    {
+                        chengjiaonum = 0;
+                    }
+                    int yishounum;
+                    if (!int.TryParse(row[Form2.yishoucolindex]?.ToString(), out yishounum))
+                    {
+                        yishounum = 0;
+                    }
+                    int skunum = 0;
+                    try
+                    {
+                        JArray ja = (JArray)JsonConvert.DeserializeObject(row[Form2.skuscolindex]?.ToString());
+                        //string str = jo["image"].ToString();
+                        if(ja.Count>0)
+                        {
+                            skunum = ja[0]["values"].Count();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+
+                    }
+                    int pinlunshu;
+                    if (!int.TryParse(row[Form2.pinluncolindex]?.ToString(), out pinlunshu))
+                    {
+                        pinlunshu = 0;
+                    }
+                    string keyword = row[Form2.keywordcolindex]?.ToString();
+
+                    //this.yiyilandbDataSet.products.AddproductsRow(id, url, title, skunum, minprice, maxprice, chengjiaonum, yishounum, pinlunshu, keyword, market);
+                    using (yiyilandbEntities dbContext = new yiyilandbEntities())
+                    {
+                        bool isnew = false;
+                        products pro = dbContext.products.Find(id);
+                        if (pro == null)
+                        {
+                            pro = new products();
+                            pro.id = id;
+                            isnew = true;
+                        }
+                        pro.url = url;
+                        pro.title = title;
+                        pro.skunum = skunum;
+                        pro.minprice = minprice;
+                        pro.maxprice = maxprice;
+                        pro.minchengjiao = chengjiaonum;
+                        pro.minyishou = yishounum;
+                        pro.review_quantity = pinlunshu;
+                        pro.keyword = keyword;
+                        pro.market = market;
+                        if(isnew)
+                        {
+                            dbContext.Set<products>().Add(pro);
+                        }
+                        dbContext.SaveChanges();
+                    }
+
+
+                    //lstkeywords.Add(keyword);
+                    //m_datatable.Rows.Add(id, title, minprice, maxprice, chengjiaonum, yishounum, skunum, pinlunshu, keyword);
+
+                }
+            }
+            //dataGridView1.DataSource = m_datatable;
+            //comboBox1.DataSource = lstkeywords.ToArray();
+        }
+
+
+        bool ParsePrice(string strprice, out decimal minprice, out decimal maxprice)
+        {
+            minprice = 0;
+            maxprice = 0;
+            try
+            {
+                var arr = strprice.Split(new char[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
+                if (arr.Length > 0)
+                {
+                    minprice = maxprice = decimal.Parse(arr[0]);
+                }
+                if (arr.Length > 1)
+                {
+                    maxprice = decimal.Parse(arr[1]);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        private void btndaoru_Click(object sender, EventArgs e)
+        {
+            DaoRu();
+        }
+
+        private void btnrefresh_Click(object sender, EventArgs e)
+        {
+            this.yiyilandbDataSet.products.Clear();
+            this.productsTableAdapter.Fill(this.yiyilandbDataSet.products);
+        }
+    }
+}
